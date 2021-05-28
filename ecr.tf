18303f477587e232fd7017494d036a1827e0d4ee
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-05-28 16:30:40 +0000

    Initial commit, adding githooks submodule

--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "cert-exporter" {
+  name = "cert-exporter"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/cert-exporter" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "cert-exporter" {
+  repository = aws_ecr_repository.cert-exporter.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.cert-exporter.repository_url
 }
